240057c58a51d4407a5614d23d77008ec197c9de
BorjaGD <borjagarciadelgado@gmail.com>
2019-08-06 21:25:42 +0100

    New button in uc_HeroSelection to go back to main menu

--- SuperAdventure/SuperAdventure/uc_HeroSelection.cs
+++ SuperAdventure/SuperAdventure/uc_HeroSelection.cs
@@ -12,9 +12,32 @@ namespace SuperAdventure
 {
     public partial class uc_HeroSelection : UserControl
     {
+
+        private static uc_HeroSelection _instance;
+
+        public static uc_HeroSelection instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new uc_HeroSelection();
+                }
+                return _instance;
+            }
+        }
         public uc_HeroSelection()
         {
             InitializeComponent();
+        }
+
+        private void heroSelection_back_button_Click(object sender, EventArgs e)
+        {
+            uc_MainMenu mainMenu = new uc_MainMenu();
+            Parent.Controls.Add(mainMenu);
+            Parent.Controls.Remove(instance);
+            mainMenu.Dock = DockStyle.Fill;
+            mainMenu.BringToFront();
         }
     }
 }
