fc197db3c567301bc6442787b16b0a6a232e4f2f
BGarciaD <borjagarciadelgado9@gmail.com>
2019-08-16 15:40:35 +0100

    Added button for options

--- SuperAdventure/SuperAdventure/uc_Game.Designer.cs
+++ SuperAdventure/SuperAdventure/uc_Game.Designer.cs
@@ -28,12 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btn_Options = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // btn_Options
+            // 
+            this.btn_Options.Location = new System.Drawing.Point(24, 18);
+            this.btn_Options.Name = "btn_Options";
+            this.btn_Options.Size = new System.Drawing.Size(75, 38);
+            this.btn_Options.TabIndex = 0;
+            this.btn_Options.Text = "Opciones";
+            this.btn_Options.UseVisualStyleBackColor = true;
             // 
             // uc_Game
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btn_Options);
             this.Name = "uc_Game";
             this.Size = new System.Drawing.Size(800, 610);
             this.ResumeLayout(false);
@@ -41,5 +52,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn_Options;
     }
 }
